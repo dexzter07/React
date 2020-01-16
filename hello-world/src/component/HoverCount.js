c081de209f5e6881e8f3491df845bafd6bdd05b0
dexzter07 <dextersharmma960@gmail.com>
2020-01-16 19:29:05 +0530

    initial COmit

---
+++ hello-world/src/component/HoverCount.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import withCounter from './withCounter'
+
+class HoverCount extends Component {
+    
+    render() {
+        const {count, incrementCount}= this.props
+        return (
+            <div>
+    <h1 onMouseOver={incrementCount}>
+        {this.props.name}
+        Hover {count} times</h1>
+            </div>
+        )
+    }
+}
+
+export default withCounter(HoverCount,5)
---
+++ hello-world/src/component/ReactFragment.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ReactFragment() {
+    return (
+        <React.Fragment>
+            <h1>Fragment Demo</h1>
+            <p>This describes the Fragment demo Component.</p>
+        </React.Fragment>
+    )
+}
+
+export default ReactFragment
+
